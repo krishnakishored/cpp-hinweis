a8def5f8aaaddc6b7fa33c063753e03ec1c11d7b
kishore.donthikurthi <kishore.donthikurthi@ispace.com>
2019-06-24 16:30:11 +0530

    Update Singleton

--- Threads/threads_6.cpp
+++ Threads/threads_6.cpp
@@ -21,21 +21,41 @@ class  Singleton{
 private:
     static std::shared_ptr<Singleton> ptr;
     static std::once_flag ptr_once;
-    Singleton(){cout << "created.. \n";}
+    int data;
+    Singleton(){
+        data = 0;
+        cout << "created.. \n";
+        }
 public:
     static std::shared_ptr<Singleton> getInstance(){
-        std::call_once(ptr_once,[](){return std::shared_ptr<Singleton>(new Singleton());});
-    
+        std::call_once(ptr_once,[](){
+            cout<<"in call_once\n";
+            // return std::shared_ptr<Singleton>(new Singleton());
+            ptr = std::shared_ptr<Singleton>(new Singleton());
+            });
+        // When this method is invoked second time, ptr is returned (nullptr)
+        cout<<"in get_instance\n";
         return ptr;
     }  
+    void print(){
+        cout<<"inside Singleton: "<<data++ <<"\n";
+    }
+
 };
 
+std::shared_ptr<Singleton> Singleton::ptr = nullptr;
+std::once_flag Singleton::ptr_once;
 
 int main()
 {
 
     // std::shared_ptr<Singleton> sptr = sptr->getInstance();
-
+    std::shared_ptr<Singleton> S1 =  Singleton::getInstance();
+    std::shared_ptr<Singleton> S2 =  Singleton::getInstance();
+    if(nullptr == S2)
+        cout<<  "It's the same object\n";
+    S1->print();
+    S2->print();
 
     return 0;
 }
